1a51ccd7edcfa944ab11aa06194d10ebd86e3d2a
Axenide <axenide@gmail.com>
2025-06-03 03:06:53 -0300

    tweak

--- modules/updater.py
+++ modules/updater.py
@@ -23,7 +23,7 @@ import config.data as data
 # File locations
 VERSION_FILE = get_relative_path("../utils/version.json")
 REMOTE_VERSION_FILE = "/tmp/remote_version.json"
-REMOTE_URL = "https://raw.githubusercontent.com/Axenide/Ax-Shell/refs/heads/dev/utils/version.json"
+REMOTE_URL = "https://raw.githubusercontent.com/Axenide/Ax-Shell/refs/heads/main/utils/version.json"
 REPO_DIR = get_relative_path("../")
 
 SNOOZE_FILE_NAME = "updater_snooze.txt"
